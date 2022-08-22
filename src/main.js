ce7e6318a74e119d86576b02dc08f747e9d523ae
Bernardo Di Leo <bernidileo@gmail.com>
2022-08-22 15:14:58 -0300

    Categories list

--- src/main.js
+++ src/main.js
@@ -21,5 +21,27 @@ async function getTrendingMoviesPreview() {
       trendingPreviewMoviesContainer.appendChild(movieContainer);
     });
   }
+
+  async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
   
-  getTrendingMoviesPreview();
+    const categories = data.genres;
+    categories.forEach(category => {
+      const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+      
+      const categoryContainer = document.createElement('div');
+      categoryContainer.classList.add('category-container');
+  
+      const categoryTitle = document.createElement('h3');
+      categoryTitle.classList.add('category-title');
+      categoryTitle.setAttribute('id', 'id' + category.id);
+      const categoryTitleText = document.createTextNode(category.name);
+
+      categoryTitle.appendChild(categoryTitleText);
+      categoryContainer.appendChild(categoryTitle);
+      previewCategoriesContainer.appendChild(categoryContainer);
+    });
+  }
+getTrendingMoviesPreview();
+getCategoriesPreview();
